e97c91f664021fa23aa12c063b848a32978a7225
JesperLarsen117 <zuqle@hotmail.com>
2020-09-14 10:52:48 +0200

    first commit, routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ErrorComponent } from './pages/error/error.component';
+import { FrontpageComponent } from './pages/frontpage/frontpage.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'forside', pathMatch: 'full' },
+  { path: 'forside', component: FrontpageComponent },
+  { path: '**', component: ErrorComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
